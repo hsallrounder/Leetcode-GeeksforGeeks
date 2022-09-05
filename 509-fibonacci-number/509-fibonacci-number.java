3bb6a3696905ac80a34b04d1472b5de82af63db0
Himanshu Sharma <hsallrounder@gmail.com>
2022-09-05 08:49:35 +0530

    Time: 6 ms (39.42%), Space: 40.9 MB (48.25%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,12 +1,7 @@
 class Solution {
-    private int f(int[] memo,int n){
-        if(memo[n]==0){
-            if(n<2) memo[n]=n;
-            else memo[n]=f(memo,n-1)+f(memo,n-2);
-        }
-        return memo[n];
-    }
     public int fib(int n) {
-        return f(new int[n+1],n);
+        if(n<=1) return n;
+        if(n==2) return 1;
+        return fib(n-1)+fib(n-2);
     }
 }
