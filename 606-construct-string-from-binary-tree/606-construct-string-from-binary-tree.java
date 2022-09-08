cfc94d201b66d7f0c5bfcbe2667ca7e31fa948d3
Himanshu Sharma <hsallrounder@gmail.com>
2022-09-08 13:47:30 +0530

    Time: 23 ms (59.89%), Space: 46.1 MB (64.69%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -16,8 +16,24 @@
 class Solution {
     public String tree2str(TreeNode root) {
         if(root==null) return "";
-        if(root.left==null && root.right==null) return root.val+"";
-        if(root.right==null) return root.val+"("+tree2str(root.left)+")";
-        return root.val+"("+tree2str(root.left)+")"+"("+tree2str(root.right)+")";
+        Stack<TreeNode> st=new Stack<>();
+        Set<TreeNode> vis=new HashSet<>();
+        st.push(root);
+        StringBuilder res=new StringBuilder();
+        while(!st.isEmpty()){
+            root=st.peek();
+            if(vis.contains(root)){
+                st.pop();
+                res.append(")");
+            }
+            else{
+                vis.add(root);
+                res.append("("+root.val);
+                if(root.left==null && root.right!=null) res.append("()");
+                if(root.right!=null) st.push(root.right);
+                if(root.left!=null) st.push(root.left);
+            }
+        }
+        return res.substring(1,res.length()-1);
     }
 }
