75015d6530126c21b0e073004d7cdfd1db808392
Himanshu Sharma <hsallrounder@gmail.com>
2022-09-22 11:43:58 +0530

    Time: 236 ms (8.96%), Space: 128.5 MB (7.81%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -1,16 +1,23 @@
 class Solution {
-    private String makeWrd(String s,int i,int j,String ans){
-        if(j<i) return ans;
-        return makeWrd(s,i,j-1,ans+s.charAt(j));
-    }
     public String reverseWords(String s) {
-        List<String> res=new ArrayList<>();
-        for(int i=0,j=0;j<s.length();j++){
-            if(j+1==s.length() || s.charAt(j+1)==' '){
-                res.add(makeWrd(s,i,j,"").trim());
-                i=j+1;
+        String res="";
+        String temp="";
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c!=' '){
+                temp=s.charAt(i)+temp;
             }
+            else{
+                if(res.length()!=0) res+=" "; 
+                res+=temp.trim();
+                temp="";
+            }
+            if(i+1==s.length()){
+                if(res.length()!=0) res+=" ";
+                res+=temp.trim();
+            }
+            
         }
-        return String.join(" ",res);
+        return res;
     }
 }
