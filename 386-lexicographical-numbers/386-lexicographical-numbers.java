8ea9f9fe727dc9d52f3260b9c1864c460e921117
Himanshu Sharma <hsallrounder@gmail.com>
2022-09-29 14:59:59 +0530

    Time: 2 ms (100.00%), Space: 49.8 MB (88.61%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.java
+++ 386-lexicographical-numbers/386-lexicographical-numbers.java
@@ -2,15 +2,18 @@ class Solution {
     private static void LexicoCount(int n,List<Integer> res,int val){
         if(val>n) return;
         //Method1
+        {
         res.add(val);
         LexicoCount(n,res,val*10);
         if(val%10!=9) LexicoCount(n,res,val+1);
-        
+        }
         //Method2
+        //{
         //if(val!=0) res.add(val);
         // for(int i=(val==0) ? 1 : 0 ; i<=9 ; i++){
-        //     LexicoCount(n,res,val*10+i);
+        // LexicoCount(n,res,val*10+i);
         // }
+        //}
     }
     public List<Integer> lexicalOrder(int n) {
         List<Integer> res=new LinkedList<>();
